3744786c6cdfcc73f76a7c9de3ff15e5e11d9fbc
aknagirm <mriganka.s.sarkar@gmail.com>
2024-04-28 12:04:50 +0530

    3rd commit about type

--- index.ts
+++ index.ts
@@ -1,8 +1,36 @@
-const character = 'Mriganka'
-console.log(character)
+let character: string = 'mario';
+let age: number;
+let isLoggedIn: boolean;
 
-const inputs = document.querySelectorAll('input')
+// age = 'luigi';
+age = 30;
 
-inputs.forEach(input => {
-    console.log(input);
-  });
+// isLoggedIn = 25;
+isLoggedIn = true;
+
+// arrays
+let ninjas: string[] = [];
+
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
+
+// union types
+let mixed: (string|number|boolean)[] = [];
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
+
+let uid: string|number;
+
+// objects
+let ninjaOne: object;
+ninjaOne = { name: 'yoshi', age: 30 };
+
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string
+};
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
